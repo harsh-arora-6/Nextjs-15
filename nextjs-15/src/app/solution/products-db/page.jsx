dd982e1afe37a9ca4db904e5806ba856cca670e7
harsh-arora-6 <arora99harsh@gmail.com>
2025-04-03 18:23:46 +0530

    solution 1

---
+++ nextjs-15/src/app/solution/products-db/page.jsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+// actions
+import { getProducts } from "@/prisma-db";
+
+export default async function ProductsDBPage() {
+  // fetch products from db directly here
+  const products = await getProducts();
+
+  return (
+    <section className="flex flex-col space-y-4 p-4">
+      <h1 className="text-3xl font-semibold">Products</h1>
+      <ul className="space-y-4">
+        {products.map((product) => (
+          <li
+            key={product.id}
+            className="p-4 bg-white text-gray-700 shadow-md rounded-lg"
+          >
+            <Link
+              href={`/products-db/${product.id}`}
+              className="text-xl font-semibold"
+            >
+              {product.title}
+            </Link>
+            <p>{product.description}</p>
+            <p className="text-lg font-medium">${product.price}</p>
+            <button className="bg-red-500 text-white px-4 rounded mt-2 py-1">
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <Link
+        href="/solution/create-product-server"
+        className="p-3 bg-blue-500 w-fit rounded-md"
+      >
+        Add product
+      </Link>
+    </section>
+  );
+}
